f384403596a667a39dcf949e2e11f40e8dda70ff
vautheman <authirard.victor@gmail.com>
2023-05-08 23:20:04 +0200

    change background

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       {/*<body className='bg-background text-white bg-[url("../../public/bg-noise.png")] bg-blend-overlay bg-repeat-round'>*/}
-      <body className='bg-background text-white bg-[url("../../public/bg-noise-filter.png")] bg-repeat bg-[length:50%]'>
+      <body className='bg-background text-white bg-[url("../../public/bg-noise-filter.png")] bg-repeat'>
         <Main />
         <NextScript />
       </body>
