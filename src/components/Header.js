2b1e32d190524f91311218b8064b00bf13cc10a2
vautheman <authirard.victor@gmail.com>
2023-05-11 17:36:24 +0200

    responsive update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
 
 
   return (
-    <header className="grid xl:grid-cols-2 container px-10 h-auto sm:h-screen mt-44 sm:mt-0">
-      <div className='flex flex-col justify-center gap-8 h-full'>
+    <header className="grid xl:grid-cols-2 container px-10 h-auto sm:min-h-screen mt-44 sm:mt-0 mb-32">
+      <div className='flex flex-col justify-center gap-8 h-full py-28'>
         <h1 className="font-title font-bold text-6xl sm:text-7xl md:text-8xl"><span className="text-secondary">Graphic</span> Design and <span className="text-primary">Front-End</span> Developer</h1>
         <p className='font-body max-w-xl text-xl text-body-secondary'>My name is Victor Autheman and I’m fascinated by the new technologies and the universe of the web more particularly</p>
         
