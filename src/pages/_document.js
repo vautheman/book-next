8115677ecb659f1c20af83734dce2f1c817a490f
vautheman <authirard.victor@gmail.com>
2023-05-15 13:09:01 +0200

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       {/*<body className='bg-background text-white bg-[url("../../public/bg-noise.png")] bg-blend-overlay bg-repeat-round'>*/}
       <body className='bg-background text-white bg-[url("../../public/bg-noise-filter.png")] bg-repeat'>
         <Main />
