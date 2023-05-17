624aa9073ae9c393c363c8d850662546fb01e070
vautheman <authirard.victor@gmail.com>
2023-05-17 11:05:55 +0200

    add google analytics scrypt

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -6,6 +7,21 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-SXERD9NT1C" strategy="afterInteractive" />
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SXERD9NT1C', {
+                page_path: window.location.pathname,
+              });
+            `,
+           }}
+         /> 
       </Head>
       {/*<body className='bg-background text-white bg-[url("../../public/bg-noise.png")] bg-blend-overlay bg-repeat-round'>*/}
       <body className='bg-background text-white bg-[url("../../public/bg-noise-filter.png")] bg-repeat'>
