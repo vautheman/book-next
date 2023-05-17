16416bdf862ce2d6e88abd6d89d8f1c59e584c8b
vautheman <authirard.victor@gmail.com>
2023-05-17 17:16:16 +0200

    update file for metachat

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,10 +7,10 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        <MetaChat /> 
       </Head>
       {/*<body className='bg-background text-white bg-[url("../../public/bg-noise.png")] bg-blend-overlay bg-repeat-round'>*/}
       <body className='bg-background text-white bg-[url("../../public/bg-noise-filter.png")] bg-repeat'>
+        <MetaChat /> 
         <Main />
         <NextScript />
       </body>
